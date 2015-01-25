029a698f8e39c7c0f7dcacf9ee2840d5c9ffc042
eyal <eyal.regular@gmail.com>
2015-01-25 21:18:42 +0200

    adding design

--- www/js/routes.js
+++ www/js/routes.js
@@ -61,6 +61,6 @@ angular.module('PickBeauty').config(function($stateProvider, $urlRouterProvider)
 		});
 
 	// if none of the above states are matched, use this as the fallback
-	$urlRouterProvider.otherwise('/tab/search');
+	$urlRouterProvider.otherwise('/tab/categories');
 
 });
